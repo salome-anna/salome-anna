12ea4500ca087a110a273d2d7f777dc9f4e406ff
salome-anna <salomewirnsperger@gmail.com>
2021-10-20 15:12:39 +0200

    20102021, versuch 3

--- Test.java
+++ Test.java
@@ -2,12 +2,12 @@
 public class Test {
 
 	public static void main(String[] args) {
-		//DEFINE VECTORS
-		Matrix4D m1 = new Matrix4D(new double[][]{{1,2,3,4},{1,2,3,4},{1,2,3,4},{1,2,3,4}});
-		Matrix4D m2 = new Matrix4D(new double[][] {{12,4,3,5},{5,8,-3,6},{2,3,0,0},{1,-1,5,-6}});
-		Matrix4D m3 = new Matrix4D(new double[][] {{2,3,4,5},{2,3,4,5},{2,3,4,5},{2,3,4,5}});
-		Matrix4D m4 = new Matrix4D(new double[][] {{3,2,-1,4},{1,5,0,2},{1,-2,4,1},{3,1,-2,5}});
-		Matrix4D m5 = new Matrix4D(new double[][] {{-3,2,0,1},{0,2,3,-2},{-1,0,3,1},{5,2,1,3}});    
+		//DEFINE MATRICES
+		MyMatrix4D m1 = new MyMatrix4D(new double[][]{{1,2,3,4},{1,2,3,4},{1,2,3,4},{1,2,3,4}});
+		MyMatrix4D m2 = new MyMatrix4D(new double[][] {{12,4,3,5},{5,8,-3,6},{2,3,0,0},{1,-1,5,-6}});
+		MyMatrix4D m3 = new MyMatrix4D(new double[][] {{2,3,4,5},{2,3,4,5},{2,3,4,5},{2,3,4,5}});
+		MyMatrix4D m4 = new MyMatrix4D(new double[][] {{3,2,-1,4},{1,5,0,2},{1,-2,4,1},{3,1,-2,5}});
+		MyMatrix4D m5 = new MyMatrix4D(new double[][] {{-3,2,0,1},{0,2,3,-2},{-1,0,3,1},{5,2,1,3}});
 
 		//DEFINE VECTORS
 		MyVector v1 = new MyVector(new double[] {1,2,3});
@@ -15,48 +15,48 @@ public class Test {
 		MyVector v3 = new MyVector(new double[] {-1,0,-4});
 		MyVector v4 = new MyVector(new double[] {1,2,3,4});
 
-		//MULTIPLY MATRICES
+		//MULTIPLY MATRIX WITH MATRIX
 		System.out.println("\nMULTIPLY MATRICES:");
-		Matrix4D mResult1 = m1.mult(m2);
-		Matrix4D mResult2 = m2.mult(m3);
-		Matrix4D mResult3 = m4.mult(m5);
+		MyMatrix4D mResult1 = m1.mult(m2);
+		MyMatrix4D mResult2 = m2.mult(m3);
+		MyMatrix4D mResult3 = m4.mult(m5);
 		mResult1.printMatrix();
 		System.out.println();
 		mResult2.printMatrix();
 		System.out.println();
 		mResult3.printMatrix();
 
-		//MULTIPLY MATRIX WITH SCALAR
+		//MULTIPLY MATRIX WITH SCALAR - not working YET
 		System.out.println("\nMULTIPLY MATRIX WITH SCALAR:");
-		Matrix4D mResult4 = m1.mult(3);
-		Matrix4D mResult5 = m2.mult(5);
-		mResult4.printMatrix;
-        mResult5.printMatrixt
+		MyMatrix4D mResult4 = m1.mult(3);
+		MyMatrix4D mResult5 = m2.mult(5);
+		mResult4.printMatrix();
 		System.out.println();
-		m2.printMatrix();
+		mResult5.printMatrix();
 
-		//MULTIPLY MATRIX WITH VECTOR
+		//MULTIPLY MATRIX WITH VECTOR - only working with 3d vector
 		System.out.println("\nMULTIPLY MATRIX WITH VECTOR:");
-		m1.mult(v4);
-		System.out.println(m1.mult(v4));
+		//MyVector4D mResult6 = m1.mult(v4);
+		//mResult6.printVector();
 
-
-		//CALCULATE INNER PRODUCT
+		//INNER PRODUCT
 		System.out.println("\nCALCULATE INNER PRODUCT:");
 		double s1 = v1.dot(v2);
 		double s2 = v2.dot(v3);
 		System.out.println("Inner product v1 & v2: " + s1);
 		System.out.println("Inner product v2 & v3: " + s2);
 
-		//CALCULATE CROSS PRODUCT
+		//CROSS PRODUCT
 		System.out.println("\nCALCULATE CROSS PRODUCT:");
-		Vector vp1 = v1.cross(v2);
-		Vector vp2 = v1.cross(v3);
-		System.out.println("Cross product v1 & v2: " + vp1.toString());
-		System.out.println("Cross product v1 & v3: " + vp2.toString());
+		MyVector vp1 = v1.cross(v2);
+		MyVector vp2 = v1.cross(v3);
+		System.out.println("Cross product v1 & v2: ");
+		vp1.printVector();
+		System.out.println("Cross product v1 & v3: ");
+		vp2.printVector();
 
 
-		//CALCULATE LENGTH OF VECTOR
+		//LENGTH OF VECTOR
 		System.out.println("\nCALCULATE LENGTH OF VECTOR:");
 		System.out.println("Length of v1: " + v1.length());
 		System.out.println("Length of v2: " + v2.length());
